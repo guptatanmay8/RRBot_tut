31f7c06df1021f00067e79cd87171ab6eab6f813
guptatanmay8 <58387084+guptatanmay8@users.noreply.github.com>
2020-05-19 19:45:45 +0530

    Update publish_angles.cpp

--- MYROBOT_control/src/publish_angles.cpp
+++ MYROBOT_control/src/publish_angles.cpp
@@ -1,17 +1,20 @@
 #include<ros/ros.h>
-#include<std_msgs/Float32.h>
+#include<std_msgs/Float64.h>
+#include<bits/stdc++.h>
 
 int main(int argc, char** argv)
 {
 	ros::init(argc,argv,"publish_angles");
 	ros::NodeHandle nh;
-	ros Publisher pub1= nh.advertise<std_msgs::Float32>("/rrbot/joint1_position_controller/command",10);
-	ros Publisher pub2= nh.advertise<std_msgs::Float32>("/rrbot/joint2_position_controller/command",10);
+	ros::Publisher pub1= nh.advertise<std_msgs::Float64&>("/rrbot/joint1_position_controller/command",10);
+	ros::Publisher pub2= nh.advertise<std_msgs::Float64&>("/rrbot/joint2_position_controller/command",10);
 	ros::Rate loopRate(30);
+	std_msgs::Float64 F1,F2;
 	float f1,f2;
-	cin>>f1>>f2;
-	pub1.publish(f1);
-	pub2.publish(f2);
+	std::cin>>f1>>f2;
+	F1.data=f1; F2.data=f2;
+	pub1.publish(F1);
+	pub2.publish(F2);
 	while(ros::ok())
 	{
 		loopRate.sleep();
